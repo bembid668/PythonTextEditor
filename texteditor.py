22f52cea435fbdff72bf4a1c87a10eb1e9f84604
Sanchit Bembi <bembid668@gmail.com>
2019-03-13 19:13:22 +0000

    Initial commit with basic text and choose

---
+++ texteditor.py
@@ -0,0 +1,21 @@
+import sys
+v=sys.python_version
+if "2.7" in v:
+    from Tkinter import *
+elif "3.3" in v or "3.4" in v:
+    from tkinter import *
+    import tkFileDialog
+root=Tk("Text Editor")
+text = Text(root)
+text.grid()
+
+def saveas():
+    global text
+    t = text.get("1.0, "end-1c")
+    savelocation = tkFileDialog.asksaveasfilename()
+    file1 = open(savelocation, "w+")
+    file1.write(t)
+    file1.close()
+
+
+root.mainloop()
